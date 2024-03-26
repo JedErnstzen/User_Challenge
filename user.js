683b4ffaf393b7abddaa9f6cd9890ab12972e6ab
Sonwabiso <sonwabisolebokgang@gmail.com>
2024-03-26 17:06:03 +0200

    fixed functions declaration and logCalc statements

--- user.js
+++ user.js
@@ -5,20 +5,20 @@ let calculated = '1'
 // Only allowed to change below
 
 
-const = logCalc () => { 
-    const isString = typeof calculated = 'numerical-string' 
-    const calculatedAsNumber = isString ? calculated : parseNumber(calculated)
-    calculated === calculatedAsNumber + 1 
+const logCalc = () => { 
+    const isString = typeof calculated == 'numerical-string' 
+    const calculatedAsNumber = isString ? calculated : Number(calculated)
+    calculated = calculatedAsNumber + 1 
 }
 
-const = calcUser () => {
+const calcUser = () => {
   logCalc
   if (calculated > 2) user = 'John'
   if (calculated > 2) state = 'requesting'
   if (calculated > 3) state = 'idle'
 }
 
-const = checkUser () => {
+const checkUser = () => {
 	if (user && state === 'requesting') {
 		console.log(`User: ${user} (${calculated})`)
 	}
